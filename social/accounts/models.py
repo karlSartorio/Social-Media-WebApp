3ad6c4b73f85482303870d3bd3d2e8373b9f2cef
karlSartorio <karlSartorio@users.noreply.github.com>
2021-02-01 00:27:19 +0000

    base project functionality & account app functionality
    within this push:
    1.  project functionality
    created the simple home page, by setting up the templates(within the project folder), created the home page view, found in the project folder views.py and lastly the url map to see this home page
    
    2 account app functionality
    
    2a. models, we used the user model django already provide from auth
    
    2b.  forms, we created the from and used the class meta to determine which attributes from the auth.user we want the user to input. we also added a method that acts like a context dict, having the user's email and username linked to a text.
    
    2c. views. we created the views for the signup page,  that inherits from the class base view called CreateView. we stablish connection for the template, form and method to redirect the user to the login page once successfully logged in.
    
    2d. we create the directory for the accounts app templates, and created two file login.html and signup.html.
    
    further development for this account app will be done in the next push.
    
    NO TESTING WAS DONE JUST YET!

--- social/accounts/models.py
+++ social/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib import auth # django built in model for users
 
 # Create your models here.
+class User(auth.models.User, auth.models.PermissionsMixin):# inhert from the auth model
+
+    def __str__(self):#string representation of the model
+        return "@{}".format(self.username)
+        # username is one of the attributes from the auth.models.User
--- social/social/settings.py
+++ social/social/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIRS =  os.path.join(BASE_DIR,'templates')#part1. set up
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'social.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIRS,],
+        'DIRS': [ os.path.join(BASE_DIR,'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join.(BASE_DIR, 'static')]#part1. set up
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]#part1. set up
